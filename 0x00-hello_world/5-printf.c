c8ee878e306222424f19deadd43c7a03e7c4183c
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-07-13 08:00:24 -0700

    a C program to print a line with a printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a line followed by a new line
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
