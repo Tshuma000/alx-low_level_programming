9d851a9dabfa4480c2204c24e4dc23374d67cce5
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-07-13 07:29:00 -0700

    C program to print a line using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
 /**
  * main - prints a sentence with puts function followed by a new line
  * return: 0
- * /
+ * 
+ */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle")
-Return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
