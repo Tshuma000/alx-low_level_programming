ad5cc87924687e78bad40964da0703fe38da84e7
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-07-14 04:09:18 -0700

    C program that prints whether n is +, - or = 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Prints whether random n is +, - or = 0
+ *
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n < 0)
+	{
+		printf("%d is negetive\n", n);
+	} else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
