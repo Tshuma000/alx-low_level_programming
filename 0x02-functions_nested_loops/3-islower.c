1adcc6d1febcea0031e1d24d6278283e6c364ed5
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-07-18 08:32:30 -0700

    program to check if character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is lowercase
+ *@c: character to be checked
+ * Return: On success 1, on failure 0.
+ */
+
+int _islower(int c)
+{
+	if (c > 91)
+	{
+		return (1);
+	}
+
+	return (0);
+}
