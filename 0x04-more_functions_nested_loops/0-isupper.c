b3f36000ac77d0eb0d1f79153fc37cd527ef86b6
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-07-20 05:17:11 -0700

    program to check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letters
+ *@c: letter to be checked
+ *
+ * Return: if sucessfull 1, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c < 91)
+	{
+		return (1);
+	}
+
+	return (0);
+}
