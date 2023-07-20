56d4ee0fcd4964487aca99558f5cfcd14d6c5f09
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-07-20 05:50:25 -0700

    check if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *@c: number to be checked
+ *
+ * Return: if successful 1, otherwise 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c == '0')
+	{
+		return (1);
+	} else if (c == '1')
+	{
+		return (1);
+	} else if (c == '2')
+	{
+		return (1);
+	} else if (c == '3')
+	{
+		return (1);
+	} else if (c == '4')
+	{
+		return (1);
+	} else if (c == '5')
+	{
+		return (1);
+	} else if (c == '6')
+	{
+		return (1);
+	} else if (c == '7')
+	{
+		return (1);
+	} else if (c == '8')
+	{
+		return (1);
+	} else if (c == '9')
+	{
+		return (1);
+	}
+	return (0);
+}
