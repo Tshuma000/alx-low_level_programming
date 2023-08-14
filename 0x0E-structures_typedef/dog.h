724a142e66a020272263094da21cd96996b3605c
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-08-14 11:22:28 -0700

    program to define a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,7 @@
  * @age: age of dog
  * @owner: owner of the dog
  *
+ * Description: longer description
  */
 
 struct dog
@@ -14,5 +15,18 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
