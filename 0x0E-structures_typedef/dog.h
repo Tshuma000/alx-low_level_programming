66ce8ccb3aca9167af704cabd1cfbde55bc19cfd
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-08-14 11:11:46 -0700

    program to define a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - keeps data of dogs.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
