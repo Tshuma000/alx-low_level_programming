2bd6746dccb38e442c9777ba40aec3e2c6436398
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-08-16 11:44:22 -0700

    Function to iterate through array using function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,17 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL && size > 0)
 	{
-		(*action)(array[i]);
+		while (i < size)
+		{
+			while (i < size)
+			{
+				action(array[i]);
+				i++;
+			}
+		}
 	}
 }
