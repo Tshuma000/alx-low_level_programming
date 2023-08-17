7d9a23e5ba78accdd00ce45f175b94ef41cefa94
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-08-17 11:23:31 -0700

    variadic program to print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print strings, followed by a new line.
+ * @separator: string to seperate strings.
+ * @n: number of strings
+ * @...: variadic arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list myNumbers;
+	char *str;
+	unsigned int i;
+
+	va_start(myNumbers, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(myNumbers, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(myNumbers);
+}
