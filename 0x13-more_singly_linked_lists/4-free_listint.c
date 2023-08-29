30c0605ed01205489320b2e2e391b3edfe65fe7c
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-08-29 12:45:27 -0700

    program to free a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+}
